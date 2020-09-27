625a161aa0a03ec017688d9ca0c942baf8294868
Aaron Williams <aaronrw4@yahoo.com>
2020-09-26 19:11:07 -0700

    Passed Final Test

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -14,8 +14,8 @@ world = World()
 # map_file = "C:/Users/aaron/Git/Graphs/projects/adventure/maps/test_line.txt"
 # map_file = "C:/Users/aaron/Git/Graphs/projects/adventure/maps/test_cross.txt"
 # map_file = "C:/Users/aaron/Git/Graphs/projects/adventure/maps/test_loop.txt"
-map_file = "C:/Users/aaron/Git/Graphs/projects/adventure/maps/test_loop_fork.txt"
-# map_file = "C:/Users/aaron/Git/Graphs/projects/adventure/maps/main_maze.txt"
+# map_file = "C:/Users/aaron/Git/Graphs/projects/adventure/maps/test_loop_fork.txt"
+map_file = "C:/Users/aaron/Git/Graphs/projects/adventure/maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph = literal_eval(open(map_file, "r").read())
@@ -88,18 +88,24 @@ def stack_fxn(current_room, current_direction):
                     room.get_room_in_direction(return_direction)
                     traversal_path.append(return_direction)
         else:
-            for new_possible_exit in new_exits:
-                new_next_room = room.get_room_in_direction(new_possible_exit)
-                if new_next_room not in visited:
-                    traversal_path.append(new_possible_exit)
-                    branch_path.append(new_possible_exit)
-                    new_stack.push(new_next_room)
-                elif new_next_room == start and dead_end is False and len(branch_path) > 2:
-                    branch_path = []
-                    traversal_path.append(new_possible_exit)
+            if room.get_room_in_direction(new_exits[0]) in visited and room.get_room_in_direction(new_exits[1]) in visited:
+                for i in range(0, len(branch_path)):
+                    last_direction = branch_path.pop()
+                    return_direction = reverse_direction(last_direction)
+                    current_room.get_room_in_direction(return_direction)
+                    traversal_path.append(return_direction)
+            else:
+                for new_possible_exit in new_exits:
+                    new_next_room = room.get_room_in_direction(new_possible_exit)
+                    if new_next_room not in visited:
+                        traversal_path.append(new_possible_exit)
+                        branch_path.append(new_possible_exit)
+                        new_stack.push(new_next_room)
+                    elif new_next_room == start and dead_end is False and len(branch_path) > 2:
+                        traversal_path.append(new_possible_exit)
+                    else:
+                        pass
                     
-                else:
-                    pass
 
 starting_room = world.starting_room
 
